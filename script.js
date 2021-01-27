b641483d2b7e697586b5936f2c3bd9c47a156606
Natalya Myunster <natalyamyunster@yahoo.com>
2021-01-27 19:44:01 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//task1
+let string = prompt("write something", "");
+
+// if( string.length > 10){
+//     alert(string.toUpperCase());
+// }else {
+//     alert(string.toLowerCase());
+// }
+
+alert (string.length > 10 ? string.toUpperCase() : string.toLowerCase());
+
+//task2
+// let string = prompt("write a word", "");
+//
+// console.log(string.reverse());
+//  if( string === string.reverse()){
+//     alert("polindrome");
+// }else {
+//     alert("not polindrome");
+// }
+
